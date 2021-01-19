7a55ecd1b6bc6e86e5a9479315255fb6e1311d0e
lkjuh <72807554+lkjuh@users.noreply.github.com>
2021-01-19 21:12:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("button")
+addEventListener(onclick,function(){
+    let x1 = Number(prompt("first"))
+    let x2 = Number(prompt("second"))
+    let x  = x1 + x2
+    alert(x)
+})
